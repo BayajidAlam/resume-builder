318d15920354eef6bb7bb6260db03b317b4d8996
BayajidAlam <bayajidalam2001@gmail.com>
2023-03-13 18:04:46 +0600

    header added

---
+++ src/components/Header/Header.js
@@ -0,0 +1,24 @@
+import React from "react";
+import styles from "./Header.module.css";
+import resume from '../../assets/resume_folder.svg';
+
+
+function Header() {
+  return (
+    <div className={styles.container}>
+      <div className={styles.left}>
+        <p className={styles.heading}>
+          A <span>Resume</span> that stands out!
+        </p>
+        <p className={styles.heading}>
+          Make your own resume. <span>It's free</span>
+        </p>
+      </div>
+      <div className={styles.right}>
+        <img src={resume} alt="resume" />
+      </div>
+    </div>
+  );
+}
+
+export default Header;

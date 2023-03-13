d6ea4076b06d40ec68bc10ef952c037a3b30fdf0
BayajidAlam <bayajidalam2001@gmail.com>
2023-03-14 00:15:24 +0600

    set up editor

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import './App.css';
-import Body from './components/Body/Body';
-import Header from './components/Header/Header';
+import "./App.css";
+import Body from "./components/Body/Body";
+import Header from "./components/Header/Header";
 
 function App() {
   return (
-    <div className='App'>
-          <Header/>
-          <Body/>
+    <div className="App">
+      <Header />
+      <Body />
     </div>
   );
 }
--- src/components/Body/Body.js
+++ src/components/Body/Body.js
@@ -1,8 +1,21 @@
 import React from "react";
 import styles from "./Body.module.css";
+import { ArrowDown } from 'react-feather';
+import Editor from "../Editor/Editor";
 
 function Body() {
+  
   const colors = ["239ce2", "#48bb78", "#0bc5ea", "#a0aec0", "#ed8936"]
+  const sections = {
+    basicInfo: 'Basic Info',
+    workExp: 'Work Experience',
+    project: 'Projects',
+    education: 'Education',
+    achievement: 'Achievements',
+    summary: 'Summary',
+    other: 'other'
+  }
+
   return (
     <div className={styles.container}>
       <p className={styles.heading}>Resume Builder</p>
@@ -16,7 +29,10 @@ function Body() {
             />
           ))}
         </div>
-        <div><button>Dwonload</button></div>
+        <div><button>Dwonload <ArrowDown/></button></div>
+      </div>
+      <div className={styles.main}>
+            <Editor/>
       </div>
     </div>
   );
